12ec34bbe0067d08e5a58eefcbf253773056de3a
Hermes Vieira Jr <hjunior@cpqd.com.br>
2018-03-01 11:03:11 -0300

    Changes the map status when loading map visualization screen.

--- src/js/views/devices/DeviceMap.js
+++ src/js/views/devices/DeviceMap.js
@@ -414,7 +414,7 @@ class DeviceMap extends Component {
           {this.state.mapquest ? (
             <PositionRenderer devices={pointList} toggleTracking={this.toggleTracking} allowContextMenu={true}/>
           ) : (
-            <div>dummy</div>
+            <div>loading</div>
           )}
           <Sidebar devices={validDevices} hideAll={this.hideAll} showAll={this.showAll} selectedDevice={this.selectedDevice} toggleDisplay={this.toggleDisplay} />
         </div>
