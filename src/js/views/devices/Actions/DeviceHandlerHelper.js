c4b13f32f7181146b030159c523aac0ccd41c881
Mariane Previde <mariane.previde@gmail.com>
2019-05-23 14:37:08 -0300

    Update DeviceHandlerHelper.js

--- src/js/views/devices/Actions/DeviceHandlerHelper.js
+++ src/js/views/devices/Actions/DeviceHandlerHelper.js
@@ -58,6 +58,7 @@ class DeviceHandlerHelper {
         });
         specializedAttrs = this.removeRepeatElementsOnArray(specializedAttrs, 'id');
         modifiedDevice.attrs = specializedAttrs;
+        
         return modifiedDevice;
     }
 
