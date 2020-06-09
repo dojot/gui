fd410c90134d14452d156b1f19e42997fdc470f4
wellitonSouza <wellitonaz@yahoo.com.br>
2020-06-09 15:09:13 -0300

    Validation for importing firmware

--- src/js/views/templates/TemplateList/Sidebar/SidebarFirmware/SidebarFirmImages.js
+++ src/js/views/templates/TemplateList/Sidebar/SidebarFirmware/SidebarFirmImages.js
@@ -37,8 +37,24 @@ class SidebarFirmImages extends Component {
     }
 
     onDrop(files, image) {
-        ImageActions.updateImageData(image.id, 'file', files);
+        if(files && Array.isArray(files)&& files.length>0){            
+            if (files[0].name && files[0].name.length > 4 ) {
+                const fileName = files[0].name.substring(
+                    files[0].name.length - 4, files[0].name.length);
+                if (fileName.toUpperCase() !== '.HEX') {
+                    const { t } = this.props;
+                    toaster.warning(t('firmware:alerts.file_error'));
+                } else {
+                    ImageActions.updateImageData(image.id, 'file', files);
+                }
+            }
+        }else{
+            toaster.warning(t('firmware:alerts.file_error'));
+        }
+
+      
     }
+
 
     toggleDeleteSidebar(e, image) {
         e.preventDefault();
