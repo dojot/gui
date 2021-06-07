653bbbd2ca11adae4c56b4d37df2a034a539ae11
Mariane Previde <mariane.previde@gmail.com>
2021-06-07 16:10:10 -0300

    fixes from codefactor

--- src/js/comms/certificates/CertificatesManager.js
+++ src/js/comms/certificates/CertificatesManager.js
@@ -8,18 +8,17 @@ class CertificatesManager {
     }
 
     async signCert(csrPEM) {
-        return await util.POST(`${baseURL}x509/v1/certificates`, { csr: csrPEM });
+        return util.POST(`${baseURL}x509/v1/certificates`, { csr: csrPEM });
     }
 
     async associateCert(fingerPrint, deviceId) {
      await util.PATCH(`${baseURL}x509/v1/certificates/${fingerPrint}`,
             {
             "belongsTo": {
-              "device": deviceId
-            }
-        });
+              "device": deviceId,
+            },
+      });
     }
-
 }
 
 const certificatesManager = new CertificatesManager();
